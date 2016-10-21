623b1f33ae121e0ee47d1e47b4c8b2c3e305bb33
Zutong <zutongli0805@gmail.com>
2016-10-21 14:19:36 +0800

    Create longestCommonPrefix.cpp

---
+++ zt/code14/longestCommonPrefix.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+    if (strs.size() == 0) return "";
+	for (int i = 0; i < strs[0].length(); i++) {
+		for (int j = 1; j < strs.size(); j++) {
+			if (strs[0][i] != strs[j][i] || i>=strs[j].length()) {
+				return strs[0].substr(0,i);
+			}
+		}
+	}
+	return strs[0];
+    }
+};
